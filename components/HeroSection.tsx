abf7df305e35cb8741a6618ee0ccc4acc2d5b54c
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-17 14:41:40 +0100

    fix imports

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
 
-export const HeroSection = () => {
+export default function HeroSection() {
   return (
 
     <section id="home">

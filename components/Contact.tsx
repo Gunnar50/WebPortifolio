0ca9d624bece76fc4104a9d940858e8d9a17ad6e
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-18 10:42:33 +0100

    finished contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,81 +3,69 @@ import Image from "next/image"
 import Link from "next/link"
 import SlideUp from "./SlideUp"
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
-import { AiOutlineMail, AiFillLinkedin, AiOutlineWhatsApp, AiOutlinePhone } from "react-icons/ai";
+import { AiOutlineMail, AiOutlineWhatsApp, AiOutlinePhone } from "react-icons/ai";
+
+const email = "gustavo.passarella@hotmail.com"
+const phoneNumber = "+447580577781"
+
+const ContactItems = [
+    {
+        label: "Call",
+        description: phoneNumber,
+        link: "tel:+447580577781",
+        icon: AiOutlinePhone,
+    },
+    {
+        label: "Email",
+        description: email,
+        link: `mailto:${email}`,
+        icon: AiOutlineMail,
+    },
+    {
+        label: "WhatsApp",
+        description: phoneNumber,
+        link: `https://wa.me/${phoneNumber}`,
+        icon: AiOutlineWhatsApp,
+    },
+]
 
 
+export default function ContactSection() {
 
 
-// export default function ContactSection() {
-//     const myEmail = "your_email@gmail.com";
-//     const myPhone = "1234567890";
-//     const myLinkedIn = "your-linkedin-url";
-  
-//     return (
-//       <section className="my-16 text-center bg-gray-800 text-white p-10">
-//         <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
-  
-//         <div className="link-list">
-//             <div className="link-col">
-//                 <h4 className="link-title">Call</h4>
-//                 <a className="link-item" href={`tel:${myPhone}`}>{myPhone}</a>
-//             </div>
+return (
+    <section id="contact" className="my-16 text-center p-10 text-white">
+        <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
 
-//             <div className="link-col">
-//                 <h4 className="link-title">Email</h4>
-//                 <a className="link-item" href={`mailto:${myEmail}`}>{myEmail}</a>
-//             </div>
-
-//             <div className="link-col">
-//                 <h4 className="link-title">WhatsApp</h4>
-//                 <a className="link-item" href={`https://wa.me/${myPhone}`}>{myPhone}</a>
-//             </div>
-
-//             <div className="link-col">
-//                 <h4 className="link-title">WhatsApp</h4>
-//                 <a className="link-item" href={myLinkedIn}>{myLinkedIn}</a>
-//             </div>
-
-//         </div>
-        
-//       </section>
-//     )
-//   }
-
-  export default function ContactSection() {
-    const myEmail = "gustavo.passarella@hotmail.com";
-    const myPhone = "+447580577781";
-  
-    return (
-        <section id="contact">
-          <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
-    
-          <div className="flex flex-col md:flex-row md:justify-between items-center md:space-x-0">
-            <div className="flex flex-col items-center text-left mb-8 md:mb-0 md:w-1/3">
-              <h4 className="font-semibold uppercase text-gray-400 mb-4">Call</h4>
-              <a href={`tel:${myPhone}`} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
-                <AiOutlinePhone size={24} />
-                <span>{myPhone}</span>
-              </a>
+        <div className="flex flex-col md:flex-row md:justify-between items-center md:space-x-0">
+            {ContactItems.map((item) => (
+                <div className="flex flex-col items-center text-left mb-8 md:mb-0 md:w-1/3">
+                <h4 className="font-semibold uppercase text-gray-400 mb-4">{item.label}</h4>
+                <a href={item.link} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
+                <item.icon size={24} />
+                <span>{item.description}</span>
+                </a>
             </div>
-    
-            <div className="flex flex-col items-center text-left mb-8 md:mb-0 md:w-1/3">
-              <h4 className="font-semibold uppercase text-gray-400 mb-4">Email</h4>
-              <a href={`mailto:${myEmail}`} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
+            ))}
+            
+
+            {/* <div className="flex flex-col items-center text-left mb-8 md:mb-0 md:w-1/3">
+                <h4 className="font-semibold uppercase text-gray-400 mb-4">Email</h4>
+                <a href={`mailto:${myEmail}`} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
                 <AiOutlineMail size={24} />
                 <span>{myEmail}</span>
-              </a>
+                </a>
             </div>
-    
+
             <div className="flex flex-col items-center text-left md:w-1/3">
-              <h4 className="font-semibold uppercase text-gray-400 mb-4">WhatsApp</h4>
-              <a href={`https://wa.me/${myPhone}`} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
+                <h4 className="font-semibold uppercase text-gray-400 mb-4">WhatsApp</h4>
+                <a href={`https://wa.me/${myPhone}`} target="_blank" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition-all link-item">
                 <AiOutlineWhatsApp size={24} />
                 <span>{myPhone}</span>
-              </a>
-            </div>
-          </div>
-        </section>
-      );
-  }
+                </a>
+            </div> */}
+        </div>
+    </section>
+    );
+}
 

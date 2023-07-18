fc57e930339b823a6bbf4a8bfd7c8916138d3fa8
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-18 10:26:15 +0100

    finishing contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection  from "@/components/HeroSection"
 import AboutSection  from "@/components/AboutSection"
 import ProjectsSection from "@/components/ProjectSection"
+import ContactSection from "@/components/Contact"
 
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       <HeroSection/>
       <AboutSection/>
       <ProjectsSection/>
-
+      <ContactSection/>
     </main>
   )
 }
